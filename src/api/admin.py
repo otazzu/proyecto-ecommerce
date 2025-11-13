3163fbce05937614e274c7c08ba40551a3af72bc
otazzu <srotazu@gmail.com>
2025-11-13 14:12:02 +0000

    admin modified

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,4 +1,4 @@
-  
+
 import os
 from flask_admin import Admin
 from api.database.db import db
@@ -9,18 +9,15 @@ from api.models.Rol import Rol
 from api.models.StripePay import StripePay
 from flask_admin.contrib.sqla import ModelView
 
+
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
-    
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Product, db.session))
-    admin.add_view(ModelView(Review, db.session))
     admin.add_view(ModelView(Rol, db.session))
-    admin.add_view(ModelView(StripePay, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
