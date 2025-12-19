2ca95da19a4f988baf903c835d9420cb925c3948
Darío Otazú <srotazu@gmail.com>
2025-12-19 13:49:35 +0000

    Upload Nabvar

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
 
   // Cerrar dropdown al hacer clic fuera
   useEffect(() => {
-    function handleClickOutside(event) {
+    const handleClickOutside = (event) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
         setIsOpen(false);
       }
@@ -59,7 +59,7 @@ export const Navbar = () => {
             {/* Resto del nav... */}
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex shrink-0 items-center">
-                <img src="src/front/assets/img/logo-kurisu-shop.png" alt="Your Company" className="h-8 w-auto" />
+                <img src="src/front/assets/img/logo-kurisu-shop.png" alt="Your Company" className="h-10 w-auto" />
               </div>
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4">
